947ab148fc7d69b02bea4cb90d98342b80a00a10
AI_Nomads <chugue85@gmail.com>
2024-03-06 03:44:27 +0900

    catching up

--- src/main/java/shop/mtcoding/blog/controller/MainController.java
+++ src/main/java/shop/mtcoding/blog/controller/MainController.java
@@ -92,5 +92,42 @@ public class MainController {
         request.setAttribute("nextPage", Math.min(totalPages, currentPage + 1));
         return "index";
     }
+
+
+    @GetMapping("/?keyword={keyword}")
+    public String index(@PathVariable String keyword, HttpServletRequest request, @RequestParam(defaultValue = "") String keyword) {
+        int currentPage = page;
+        boolean lastPage = paging.lastPage(currentPage);
+        boolean firstPage = paging.firstPage(currentPage);
+        int totalPages = paging.totalPages();
+
+        List<Jobs> pageList;
+        List<Page> pageActive = new ArrayList<>();
+
+        // 페이지 네이션에 현재페이지 active기능 넣기
+        for (int i = 1; i <= totalPages; i++) {
+            Page page1 = new Page();
+            page1.setNumber(i);
+            page1.setActive(currentPage == i);
+            pageActive.add(page1);
+        }
+
+        // 한 페이지에 5개씩 출력 - 10개중 현재페이지가 1이면 10부터 6까지, 현재 페이지가 2면 5부터 1까지
+
+        if (keyword.isBlank()) { //isBlank면 검색 안함
+            pageList = paging.showPages(currentPage);
+            request.setAttribute("keyword", "");
+        } else {    //검색하면 키워드를 던져줌
+            pageList = paging.showPages(currentPage, keyword);
+            request.setAttribute("keyword", keyword);
+        }
+        request.setAttribute("pages", pageActive);
+        request.setAttribute("firstPage", firstPage);
+        request.setAttribute("lastPage", lastPage);
+        request.setAttribute("pageList", pageList);
+        request.setAttribute("prevPage", Math.max(1, currentPage - 1));
+        request.setAttribute("nextPage", Math.min(totalPages, currentPage + 1));
+        return "index";
+    }
 }
 
